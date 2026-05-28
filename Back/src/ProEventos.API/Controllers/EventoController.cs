4e27eea9c0c746b752bd97e0a12fc8f35764b83f
Pedro Henrique Damasceno Dias <pedro.damasceno.dias@avanade.com>
2026-05-28 18:37:43 -0300

    Sessão 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly IEnumerable<Evento> _evento = new Evento[] {
+            new Evento() { EventoId = 1, Tema = "Angular e .NET", Local = "Belo Horizonte", DataEvento = "2024-06-01", QtdPessoas = 250, Lote = "1º Lote", ImagemURL = "foto.png" },
+            new Evento() { EventoId = 2, Tema = "Angular e .NET", Local = "São Paulo", DataEvento = "2024-07-01", QtdPessoas = 350, Lote = "2º Lote", ImagemURL = "foto.png" }
+        };
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+         [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(e => e.EventoId == id);
+        }
+    }
+}
